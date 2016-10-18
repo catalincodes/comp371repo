90f651447cff1c987f1a386e9714ac8f19a6b82e
Catalin Popescu <catalinp86@gmail.com>
2016-10-18 15:39:13 -0400

    functional build, makes a vector of Line objects. each Line has a Start vec3 and an End vec3, building a spline from point 2 to 3 out of a given first 4 points

--- asg2/VS2013_Solution/comp371lab1/asg2.cpp
+++ asg2/VS2013_Solution/comp371lab1/asg2.cpp
@@ -16,8 +16,20 @@
 #include "DataContainer.h"
 #include "CatGLEngine.h"
 
-glm::vec3 catmullRomFunc(GLfloat u, glm::mat4 controlMatrix);
-void subDivisionAlgo(GLfloat u0, GLfloat u1, GLfloat maxLineLength, glm::mat4 currentControlMatrix);
+glm::vec3 catmullRomFunc(GLfloat u, const glm::mat4& controlMatrix);
+void subDivisionAlgo(GLfloat u0, GLfloat u1, GLfloat maxLineLength, const glm::mat4& currentControlMatrix);
+struct Line
+{
+	glm::vec3 start;
+	glm::vec3 end;
+
+	Line() {}
+	Line(glm::vec3 s, glm::vec3 e):
+		start(s), end(e) { }
+
+};
+
+vector<Line> listLines;
 
 typedef unsigned char uchar; // uchar = unsigned char (0..255)
 
@@ -128,8 +140,19 @@ int main()
 
 		case RENDER_SPLINE:
 		{
+			vector<GLfloat> v = container->getData();
 			if (container->getData().size() >= 4 && booya == false) {
-				//generateSpline();
+				
+				int i = 0;
+				glm::mat4 ccm = glm::mat4(
+					glm::vec4(v[i],        v[i+1],       v[i+2],           0),
+					glm::vec4(v[i+6],      v[i+7],       v[i+8],           0),
+					glm::vec4(v[i+12],     v[i+13]      ,v[i+14],          0),
+					glm::vec4(v[i+18],     v[i+19]      ,v[i+20],          0)
+					);
+
+				subDivisionAlgo(0.0f, 1.0f, 2.0f, ccm);
+
 				booya = true;
 			}
 			break;
@@ -164,72 +187,99 @@ int main()
 	glfwTerminate();
 	return 0;
 }
-/*
-void subDivisionAlgo(GLfloat u0, GLfloat u1, GLfloat maxLineLength, glm::mat4 currentControlMatrix)
+
+void subDivisionAlgo(GLfloat u0, GLfloat u1, GLfloat maxLineLength, const glm::mat4& currentControlMatrix)
 {
+	std::cout << "subDivAlgo method called for " << u0 << " and "<< u1 << "." << std::endl;
 	GLfloat uMid = (GLfloat)((u0 + u1) / 2.0f);
 	glm::vec3 x0 = catmullRomFunc(u0, currentControlMatrix);
+	glm::vec3 x1 = catmullRomFunc(u1, currentControlMatrix);
+
+	GLfloat distance = glm::distance(x0, x1);
+	std::cout << "Back in subDivAlgo. Distance = " << distance << std::endl;
+	std::cout << "Comparing to the maxLineLength " << maxLineLength << "...";
+
+
+	if (abs(distance) > maxLineLength) {
+		std::cout << "I'm going to subdivide again with the midpoint " << uMid << "." << std::endl;
+		
+		std::cout << "Calling subdivision from " << u0 << " and " << uMid << ":" << std::endl;
+		subDivisionAlgo(u0, uMid, maxLineLength, currentControlMatrix);
+		std::cout << "Calling subdivision from " << uMid << " and " << u1 << ":" << std::endl;
+		subDivisionAlgo(uMid, u1, maxLineLength, currentControlMatrix);
+	}
+	else {
+		std::cout << "And we are good... I have the following two points to place in a container: " << std::endl;
+		std::cout << "x0 (" << x0.x << ", " << x0.y << ", " << x0.z << ") + ";
+		std::cout << "x1 (" << x1.x << ", " << x1.y << ", " << x1.z << ")" << std::endl;
+		listLines.push_back(Line(x0, x1));
+	}
+
 }
 
-*/
 
-glm::vec3 catmullRomFunc(GLfloat u, glm::mat4 controlMatrix)
+
+glm::vec3 catmullRomFunc(GLfloat u, const glm::mat4& controlMatrix)
 {
-	vector<GLfloat> v = container->getData();
+	std::cout << "CatMull calculation for : " << u << "." << std::endl;
 
-	//GLfloat u = 1;
+	/* 
+		[0][0]	[0][1]	[0][2]	[0][3]
+		[1][0]	[1][1]	[1][2]	[1][3]
+		[2][0]	[2][1]	[2][2]	[2][3]
+		[3][0]	[3][1]	[3][2]	[3][3]
+	*/
+	
 	GLfloat u_2 = pow(u, 2);
 	GLfloat u_3 = pow(u, 3);
 
 	GLfloat s = 0.5; //tension parameter
+	glm::mat4 basis = glm::mat4(
+		glm::vec4(-s    , 2 - s , s - 2     , s  ),
+		glm::vec4(2 * s , s - 3 , 3 - 2 * s , -s ), 
+		glm::vec4(-s    , 0     , s         , 0  ),
+		glm::vec4(0     , 1     , 0         , 0  )
+		);
 
 	// result of multiplying the input [u^3, u^2, u^1, 1] by the basis matrix 
-	GLfloat b1 = u_3*(0 - s) + u_2 * (2 * s) + u * (0 - s);
-	GLfloat b2 = u_3*(2 - s) + u_2 * (s - 3) + 1;
-	GLfloat b3 = u_3*(s - 2) + u_2 * (3 - 2 * s) + u*s;
-	GLfloat b4 = u_3*(s) + u_2*(0 - s);
-	
+	GLfloat b1 = u_3 * basis[0][0]   +
+				 u_2 * basis[1][0] +
+				 u   * basis[2][0]   +
+				 1   * basis[3][0];
+
+	GLfloat b2 = u_3 * basis[0][1] +
+				 u_2 * basis[1][1] +
+				 u   * basis[2][1] +
+				 1   * basis[3][1];
+
+	GLfloat b3 = u_3 * basis[0][2] +
+				 u_2 * basis[1][2] +
+				 u   * basis[2][2] +
+				 1   * basis[3][2];
+
+	GLfloat b4 = u_3 * basis[0][3] +
+				 u_2 * basis[1][3] +
+				 u   * basis[2][3] +
+				 1   * basis[3][3];
+
+
 	// multiply the vector from the previous calculation [b1, b2, b3, b4] by the control matrix
-	GLfloat x = b1*v.at(0) + b2*v.at(6) + b3*v.at(12) + b4*v.at(18);
-	GLfloat y = b1*v.at(1) + b2*v.at(7) + b3*v.at(13) + b4*v.at(19);
-	GLfloat z = b1*v.at(2) + b2*v.at(8) + b3*v.at(14) + b4*v.at(20);
+	GLfloat x = b1 * controlMatrix[0][0] + 
+				b2 * controlMatrix[1][0] +
+				b3 * controlMatrix[2][0] + 
+				b4 * controlMatrix[3][0];
+	
+	GLfloat y = b1 * controlMatrix[0][1] +
+				b2 * controlMatrix[1][1] +
+				b3 * controlMatrix[2][1] +
+				b4 * controlMatrix[3][1];
 
-	/*
-	glm::mat4 control = glm::mat4(
-		glm::vec4(v.at(0), v.at(1), v.at(2), 0),
-		glm::vec4(v.at(6), v.at(7), v.at(8), 0),
-		glm::vec4(v.at(12), v.at(13), v.at(14), 0),
-		glm::vec4(v.at(18), v.at(19), v.at(20), 0)
-		);
-	
-	std::cout << "CONTROL:" << std::endl;
-	std::cout << control[0][0] << " " << control[0][1] << " " << control[0][2] << " " << control[0][3] << std::endl;
-	std::cout << control[1][0] << " " << control[1][1] << " " << control[1][2] << " " << control[1][3] << std::endl;
-	std::cout << control[2][0] << " " << control[2][1] << " " << control[2][2] << " " << control[2][3] << std::endl;
-	std::cout << control[3][0] << " " << control[3][1] << " " << control[3][2] << " " << control[3][3] << std::endl;
+	GLfloat z = b1 * controlMatrix[0][2] +
+				b2 * controlMatrix[1][2] +
+				b3 * controlMatrix[2][2] +
+				b4 * controlMatrix[3][2];
 
-//	GLfloat s = 0.5;
-	glm::mat4 basis = glm::mat4(
-		glm::vec4(-s, 2 - s, s - 2, s),
-		glm::vec4(2 * s, s - 3, 3 - 2 * s, -s),
-		glm::vec4(-s, 0, s, 0),
-		glm::vec4(0, 1, 0, 0)
-		);
-	
-	std::cout << "BASIS:" << std::endl;
-	std::cout << basis[0][0] << " " << basis[0][1] << " " << basis[0][2] << " " << basis[0][3] << std::endl;
-	std::cout << basis[1][0] << " " << basis[1][1] << " " << basis[1][2] << " " << basis[1][3] << std::endl;
-	std::cout << basis[2][0] << " " << basis[2][1] << " " << basis[2][2] << " " << basis[2][3] << std::endl;
-	std::cout << basis[3][0] << " " << basis[3][1] << " " << basis[3][2] << " " << basis[3][3] << std::endl;
 
-	
-	//GLfloat u = 0;
-	glm::vec4 input = glm::vec4(pow(u, 3), pow(u, 2), u, 1);
-	std::cout << "INPUT:" << std::endl;
-	std::cout << input[0] << " " << input[1] << " " << input[2] << " " << input[3] << std::endl;
-	
-	glm::vec4 result = input * basis * control;
-	*/
 	std::cout << "RESULT:" << std::endl;
 	std::cout << "x = " << x << " y = " << y << " z = " << z << std::endl;
 
