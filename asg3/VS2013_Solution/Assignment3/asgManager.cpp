718a76e2788cd6cccb35efed1785908e79c724fe
Catalin Popescu <catalinp86@gmail.com>
2016-11-04 00:11:27 -0400

    creates an image based on the camera, has method that gets a vec3 of the world's x,y,z based on given x, y  (pixel in image), outputs pixels to a file (time-consuming)

--- asg3/VS2013_Solution/Assignment3/asgManager.cpp
+++ asg3/VS2013_Solution/Assignment3/asgManager.cpp
@@ -7,7 +7,34 @@ asgManager::asgManager()
 
 
 asgManager::~asgManager()
-{}
+{
+	delete image;
+}
+
+glm::vec3 asgManager::getGrid3DPos(int xpos, int ypos)
+{
+	Camera* camera = objHolder.getCamera();
+
+	if (camera) {
+		double x, y;
+
+		double f = camera->getFocalLen();
+		double a = camera->getAspectRatio();
+		double fov = camera->getFov();
+
+		double interpolant = -1.0 + (2.0*((double)xpos / ((double)imgWidth - 1.0)));
+		x = interpolant * f * a * tan((fov / 2.0)*3.1415 / 180);
+
+		interpolant = -1.0 + (2.0*((double)ypos / ((double)imgHeight - 1.0)));
+		y = interpolant * f * tan((fov / 2.0)*3.1415 / 180);
+
+		return glm::vec3(x, y, -f);
+	}
+	else {
+		return glm::vec3(0.0f);
+	}
+}
+
 
 void asgManager::execute()
 {
@@ -15,7 +42,7 @@ void asgManager::execute()
 	std::cout << "Input file name: ";
 	std::cin >> filename;
 
-	ObjectHolder objHolder;
+	//ObjectHolder objHolder;
 	Parser parse(&objHolder, filename);
 	
 	std::cout << "Reading file...";
@@ -34,7 +61,22 @@ void asgManager::execute()
 
 
 	Camera* cam = objHolder.getCamera();
-	int width = 0, height = 0;
-	cam->getImageSize(width, height);
-	std::cout << "width = " << width << " height = " << height << std::endl;
+	
+	cam->getImageSize(imgWidth, imgHeight);
+	std::cout << "width = " << imgWidth << " height = " << imgHeight << std::endl;
+
+	image = new cimg_library::CImg<double>(imgWidth, imgHeight, 0, 3, 0);
+
+	out = std::ofstream("o_" + filename);
+	for (int ypos = 0; ypos < imgHeight;++ypos) {
+		for (int xpos = 0; xpos < imgWidth;++xpos) {
+			glm::vec3 result = getGrid3DPos(xpos, ypos);
+			double x = result.x;
+			double y = result.y;
+			double z = result.z;
+			out << "(" << x << ", " << y << ", " << z << ")   ";
+		}
+		out << std::endl;
+	}
+	out.close();
 }
--- asg3/VS2013_Solution/Assignment3/asgManager.h
+++ asg3/VS2013_Solution/Assignment3/asgManager.h
@@ -2,10 +2,19 @@
 
 #include "Parser.h"
 #include "ObjectHolder.h"
+#include <fstream>
+#include "..\CImg\CImg.h"
 #include <iostream>
 
 class asgManager
 {
+	ObjectHolder objHolder;
+	int imgWidth, imgHeight;
+	cimg_library::CImg<double>* image;
+
+	std::ofstream out;
+
+	glm::vec3 getGrid3DPos(int xpos, int ypos);
 
 public:
 	
